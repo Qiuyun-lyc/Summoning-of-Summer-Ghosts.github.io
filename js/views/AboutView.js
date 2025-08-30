60e288d09d73f3dd1df501a9592fe6b8acd18fca
An0hana <7972553@gmail.com>
2025-08-30 15:35:52 +0800

    fix 关于页面、主菜单页面

--- js/views/AboutView.js
+++ js/views/AboutView.js
@@ -16,7 +16,45 @@ const AboutView = {
                 </nav>
 
                 <div class="about-content">
-                    <iframe src="./关于.html" frameborder="0"></iframe>
+                    <div class="about-content-wrapper">
+                        <!-- 第一排 2 个，居中 -->
+                        <div class="gallery row1">
+                            <div class="icon_img">
+                                <div class="flip-box">
+                                    <img class="zhengImg" src="./photo/1.jpg" alt="正面图" />
+                                    <img class="fanImg" src="./photo/1.jpg" alt="反面图" />
+                                </div>
+                            </div>
+                            <div class="icon_img">
+                                <div class="flip-box">
+                                    <img class="zhengImg" src="./photo/2.png" alt="正面图" />
+                                    <img class="fanImg" src="./photo/2.png" alt="反面图" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- 第二排 3 个，均分 -->
+                        <div class="gallery row2">
+                            <div class="icon_img">
+                                <div class="flip-box">
+                                    <img class="zhengImg" src="./photo/3.jpg" alt="正面图" />
+                                    <img class="fanImg" src="./photo/3.jpg" alt="反面图" />
+                                </div>
+                            </div>
+                            <div class="icon_img">
+                                <div class="flip-box">
+                                    <img class="zhengImg" src="./photo/4.jpg" alt="正面图" />
+                                    <img class="fanImg" src="./photo/4.jpg" alt="反面图" />
+                                </div>
+                            </div>
+                            <div class="icon_img">
+                                <div class="flip-box">
+                                    <img class="zhengImg" src="./photo/5.jpg" alt="正面图" />
+                                    <img class="fanImg" src="./photo/5.jpg" alt="反面图" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         `;
